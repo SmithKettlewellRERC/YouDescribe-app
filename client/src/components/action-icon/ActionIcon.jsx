2b0c244b13e0759c22224f6703b45f44650fbe32
Rodrigo Leme <lemerodrigo@gmail.com>
2017-03-10 14:14:05 -0800

    Moved recorder to the app and started to implement the rec actions buttons

--- client/src/components/action-icon/ActionIcon.jsx
+++ client/src/components/action-icon/ActionIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ActionIcon = (props) => {
   return (
     <div id="action-icon" className="">
-      <i className={`fa ${props.icon}`} icon="blah" id={props.id} aria-hidden="true" onClick={props.actionClick}></i>
+      <i className={`fa ${props.icon}`} icon="blah" id={props.id} aria-hidden="true" onClick={props.recordAudioClip}></i>
     </div>
   )
 };
--- client/src/containers/authoring-tool/AuthoringTool.jsx
+++ client/src/containers/authoring-tool/AuthoringTool.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import VideoPlayer from '../../components/video-player/VideoPlayer.jsx';
 import Notes from '../../components/notes/Notes.jsx';
 import Editor from '../../components/editor/Editor.jsx';
 
-const AuthoringTool = (props) => {
-  return (
-    <main id="authoring-tool">
-      <div className="w3-row-padding">
-        <div className="w3-col l8 m8 w3-margin-top">
-          <div id="notes" className="w3-card-2 w3-black w3-hide-small w3-hide-medium">
-            <VideoPlayer videoId={props.params.videoId} />
+class AuthoringTool extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    initAudioRecorder();
+  }
+
+  render() {
+    return (
+      <main id="authoring-tool">
+        <div className="w3-row-padding">
+          <div className="w3-col l8 m8 w3-margin-top">
+            <div id="notes" className="w3-card-2 w3-black w3-hide-small w3-hide-medium">
+              {/*<VideoPlayer videoId={props.params.videoId} />*/}
+            </div>
+          </div>
+          <div className="w3-col l4 m4 w3-margin-top w3-hide-small w3-hide-medium">
+            <Notes />
           </div>
         </div>
-        <div className="w3-col l4 m4 w3-margin-top w3-hide-small w3-hide-medium">
-          <Notes />
+        <div className="w3-row-padding w3-margin-top w3-hide-small w3-hide-medium">
+          <div className="w3-col w3-margin-bottom">
+            <Editor {...this.props} />
+          </div>
         </div>
-      </div>
-      <div className="w3-row-padding w3-margin-top w3-hide-small w3-hide-medium">
-        <div className="w3-col w3-margin-bottom">
-          <Editor {...props} />
+        <div id="mobile" className="w3-hide-large">
+          <center>Authoring is only available for desktop at this time</center>
         </div>
-      </div>
-      <div id="mobile" className="w3-hide-large">
-        <center>Authoring is only available for desktop at this time</center>
-      </div>
-    </main>
-  );
-};
-
+      </main>
+    );
+  }
+}
 export default AuthoringTool;
