fec4bb828fb38aafbe03326bb3fdb898534edce4
Curt Toppel <ctoppel@aol.com>
2017-03-19 18:13:29 -0700

    fixed merge conflicts in video page

--- client/src/containers/video-page/VideoPage.jsx
+++ client/src/containers/video-page/VideoPage.jsx
@@ -1,337 +1,24 @@
 import React, { Component } from 'react';
 import { Howl } from 'howler';
-<<<<<<< HEAD
 import Slider from '../../components/slider/Slider.jsx';
-
-=======
-
 import DescriberChooser from '../../components/describer-chooser/DescriberChooser.jsx';
 
->>>>>>> videoPage
 const conf = require('../../shared/config')();
 
 class VideoPage extends Component {
   constructor(props) {
     super(props);
     this.watcher = null;
-<<<<<<< HEAD
-    this.nextAudioClip = null;
-    this.currentClip = null;
-    this.previousAudioClip = null;
-    this.videoDurationInSeconds = -1;
-    this.videoState = -1;
-    this.extendedAudioClipPlaying = false;
-=======
     this.videoState = -1;
     // this.previousClip = {};
     this.currentClip = {};
     this.timeAtPause = 0;
     this.audioClipsCopy = {};
-    // this.nextAudioClip = null;
-    // this.currentClip = null;
-    // this.previousAudioClip = null;
->>>>>>> videoPage
 
     this.state = {
       videoId: props.params.videoId,
       videoUrl: `${conf.apiUrl}/videos/${props.params.videoId}`,
-<<<<<<< HEAD
-      notes: '',
 
-      videoData: {},
-      videoPlayer: null,
-      videoState: -1,
-      audioClips: [],
-      videoDuration: 0,
-      videoTitle: '',
-      videoDescription: '',
-      currentVideoProgress: 0,
-      videoDurationToDisplay: '',
-      playheadPosition: 0,
-      playheadTailHeight: 0,
-      currentTimeInVideo: 0,
-      volume: 0,
-    };
-    this.getState = this.getState.bind(this);
-    this.updateState = this.updateState.bind(this);
-    this.changeVolume = this.changeVolume.bind(this);
-  }
-
-  // 1
-  componentDidMount() {
-    this.fetchVideoData();
-  }
-
-  // 2
-  fetchVideoData() {
-    console.log('2 -> fetchingVideoData');
-    const self = this;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', this.state.videoUrl, true);
-    xhr.onload = function () {
-      if (xhr.readyState === 4) {
-        const response = JSON.parse(xhr.response);
-        const result = response.result
-          ? response.result
-          : {};
-        self.setState({
-          videoData: result,
-        }, () => {
-          self.parseVideoData();
-        });
-      }
-    };
-    xhr.send();
-  }
-
-  // 3
-  parseVideoData() {
-    console.log('3 -> parseVideoData');
-    const videoData = Object.assign({}, this.state.videoData);
-    const audioClips = [];
-    if (videoData && videoData.audio_descriptions && videoData.audio_descriptions['1'].clips) {
-      const clips = videoData.audio_descriptions['1'].clips;
-      const clipsIds = Object.keys(clips);
-      clipsIds.forEach((id) => {
-        const obj = clips[id];
-        obj.url = `${conf.audioClipsUploadsPath}${obj.file_path}/${obj.file_name}`;
-        audioClips.push(obj);
-      });
-      videoData.audio_descriptions['1'].clips = audioClips;
-    }
-    this.setState({
-      videoData,
-      audioClips,
-      playheadTailHeight: audioClips.length <= 7
-        ? audioClips.length * 27
-        : 189,
-    }, () => {
-      this.preLoadAudioClips();
-    });
-  }
-
-  // 4
-  preLoadAudioClips() {
-    console.log('4 -> preLoadAudioClips');
-    const self = this;
-    if (this.state.audioClips.length > 0) {
-      const promises = [];
-      this.state.audioClips.forEach((audioObj, idx) => {
-        console.log(idx + 1, 'audio description loaded', audioObj.url);
-        promises.push(fetch(audioObj.url));
-      });
-      Promise.all(promises).then(function() {
-        console.log('All audios loaded');
-        self.initVideoPlayer();
-      })
-      .catch(function(errorAllAudios) {
-        console.log('ERROR LOADING AUDIOS -> ', errorAllAudios);
-      });
-    } else {
-      self.initVideoPlayer();
-    }
-  }
-
-  // 5
-  initVideoPlayer() {
-    const self = this;
-    console.log('5 -> initVideoPlayer', this.state.videoId);
-    if (YT.loaded) {
-      startVideo();
-    } else {
-      window.onYouTubeIframeAPIReady = () => {
-        startVideo();
-      };
-    }
-
-    function onVideoPlayerReady() {
-    }
-
-    function onPlayerStateChange(event) {
-      self.videoState = event.data;
-      self.setState({ videoState: event.data }, () => {
-        switch (event.data) {
-          case 1:
-            clearInterval(self.watcher);
-            if (self.currentClip) {
-              self.currentClip.stop();
-            }
-            self.currentClip = null;
-            self.watcher = null;
-
-            self.videoProgressWatcher();
-            break;
-          case 2:
-            console.log('paused: 2 ')
-            if (!self.extendedAudioClipPlaying) {
-              clearInterval(self.watcher);
-              if (self.currentClip) {
-                self.currentClip.stop();
-              }
-              self.currentClip = null;
-              self.watcher = null;
-            }
-            break;
-          case 3:
-            console.log('paused: 3 ')
-            if (!self.extendedAudioClipPlaying) {
-              clearInterval(self.watcher);
-              if (self.currentClip) {
-                self.currentClip.stop();
-              }
-              self.currentClip = null;
-              self.watcher = null;
-            }
-          default:
-            console.log('default');
-        }
-      });
-    }
-
-    function startVideo() {
-      if (self.state.videoPlayer === null) {
-        self.setState({
-          videoPlayer: new YT.Player('playerAT', {
-            height: '100%',
-            videoId: self.state.videoId,
-            enablejsapi: true,
-            fs: 0,
-            rel: 0,
-            controls: 2,
-            disablekb: 1,
-            events: {
-              onReady: onVideoPlayerReady,
-              onStateChange: onPlayerStateChange,
-            },
-          }),
-        });
-      }
-    }
-  }
-
-  videoProgressWatcher() {
-    console.log('7 -> videoProgressWatcher')
-
-    let interval = 50;
-    let previousTime = 0;
-    let nextAudioClipStartTime;
-    let previousAudioClipStartTime;
-    let type;
-    let duration;
-    // this.extendedAudioClipPlaying = true;
-
-    if (this.watcher) {
-      clearInterval(this.watcher);
-      this.watcher = null;
-    }
-
-    let once = true;
-    let temp = true;
-    this.watcher = setInterval(() => {
-      const currentVideoProgress = this.state.videoPlayer.getCurrentTime();
-      console.log(currentVideoProgress);
-
-
-      if (once) {
-        this.getNextAudioClip(currentVideoProgress);
-        once = false;
-      }
-
-      this.setState({
-        currentVideoProgress,
-      });
-
-      // get that next audio clip right at 0;
-
-
-      if (this.nextAudioClip) {
-        nextAudioClipStartTime = Number(this.nextAudioClip.start_time);
-      } else {
-        nextAudioClipStartTime = Infinity;
-      }
-
-      if (this.previousAudioClip) {
-        previousAudioClipStartTime = Number(this.previousAudioClip.start_time);
-        type = this.previousAudioClip.playback_type;
-        duration = this.previousAudioClip.duration;
-      } else {
-        previousAudioClipStartTime = Infinity;
-        type = 'None';
-        duration = 0;
-      }
-
-      console.log('previous audio clip start time: ', previousAudioClipStartTime,'type: ', type, 'duration: ', duration, 'and the next audio clip start time: ',nextAudioClipStartTime)
-
-      // let the inline video is played once
-      if ((currentVideoProgress - previousAudioClipStartTime) > duration) {
-        temp = true;
-      }
-
-      if (((currentVideoProgress - previousAudioClipStartTime) < duration) && type === 'inline' && temp) {
-        if (this.currentClip) {
-          this.currentClip.stop();
-        }
-        this.currentClip = null;
-
-        this.currentClip = new Howl({
-          src: [this.previousAudioClip.url],
-          html5: true,
-        });
-        const playing = this.currentClip.play();
-        this.currentClip.seek(currentVideoProgress - previousAudioClipStartTime, playing);
-        temp = false;
-      }
-
-
-      if (currentVideoProgress > nextAudioClipStartTime) {
-        const url = this.nextAudioClip.url;
-        if (this.currentClip) {
-          this.currentClip.stop();
-        }
-        this.currentClip = null;
-
-        if (this.nextAudioClip.playback_type === 'inline') {
-          console.log('### INLINE ###', url);
-          // pause the previous video, otherwise the playback gonna keep playing
-          if (this.currentClip) {
-            this.currentClip.pause();
-          }
-          this.currentClip = new Howl({ src: [url], html5: true });
-          this.currentClip.play();
-          // let playing = this.currentClip.play();
-          // this.currentClip.seek(2, playing)
-        } else {
-          console.log('### EXTENDED ###', url);
-          // pause the previous video, otherwise the playback gonna keep playing
-          if (this.currentClip) {
-            this.currentClip.pause();
-          }
-          this.state.videoPlayer.pauseVideo();
-          this.extendedAudioClipPlaying = true;
-          this.currentClip = new Howl({
-            src: [url],
-            html5: true,
-            onend: () => {
-              this.extendedAudioClipPlaying = false;
-              this.state.videoPlayer.playVideo();
-            },
-          });
-          this.currentClip.play();
-        }
-        this.getNextAudioClip(currentVideoProgress);
-      }
-
-    }, 50);
-  }
-
-  // 7
-
-  componentWillUnmount() {
-    console.log('leaving the page');
-    clearInterval(this.watcher);
-    if (this.currentClip) {
-      this.currentClip.stop();
-=======
       currentVideoDescriber: '',
 
       // Video controls and data.
@@ -339,11 +26,6 @@ class VideoPage extends Component {
       audioClips: [],
       videoPlayer: null,
       videoState: -1,
-      // videoDuration: 0,
-      // videoTitle: '',
-      // videoDescription: '',
-      // currentVideoProgress: 0,
-      // currentTimeInVideo: 0,
     };
     this.getState = this.getState.bind(this);
     this.updateState = this.updateState.bind(this);
@@ -514,15 +196,9 @@ class VideoPage extends Component {
         currentVideoProgress,
       });
 
-      // if (this.previousClip && (currentVideoProgress > this.previousClip.end_time)) this.currentClip.stop();
         for (let i = 0; i < this.audioClipsCopy.length; i += 1) {
-          // console.log(Math.abs(+this.audioClipsCopy[i].start_time - currentVideoProgress));
-          // console.log(.25);
-          // if (Math.abs(+this.audioClipsCopy[i].start_time - currentVideoProgress) <= .25) {
-          // if (currentVideoProgress >= +this.audioClipsCopy[i].start_time && currentVideoProgress < +this.audioClipsCopy[i].end_time) {
             switch (this.audioClipsCopy[i].playback_type) {
               case 'inline':
-                // console.log('INLINE TRY');
                 if (currentVideoProgress >= +this.audioClipsCopy[i].start_time && currentVideoProgress < +this.audioClipsCopy[i].end_time) {
                   console.log('## INLINE ##');
                   this.currentClip = new Howl({
@@ -546,18 +222,11 @@ class VideoPage extends Component {
                     onend: () => {
                       this.state.videoPlayer.playVideo();
                       this.previousClip = this.currentClip;
-                      // setTimeout(() => this.previousClip = {}, interval + 200);
                     },
                     onload: () => {
-                      // console.log('ON LOAD ####', this.previousClip._src, this.currentClip._src)
                       this.currentClip.playbackType = 'extended';
                       this.audioClipsCopy = this.audioClipsCopy.slice(i + 1);
-                      // if (this.previousClip._src !== this.currentClip._src) {
-                        console.log('GOT THERE');
-                        this.currentClip.play();
-                      // } else {
-                      //   console.log('ELSE')
-                      // }
+                      this.currentClip.play();
                     },
                     onloaderror: (errToLoad) => {
                       console.log('Impossible to load', errToLoad)
@@ -572,10 +241,7 @@ class VideoPage extends Component {
               default:
                 console.log('Audio clip format not labelled or incorrect');
             }
-            // break;
-          // }
         }
-      // }
     }, interval);
   }
 
@@ -584,7 +250,6 @@ class VideoPage extends Component {
     if (this.watcher) {
       clearInterval(this.watcher);
       this.watcher = null;
->>>>>>> videoPage
     }
     this.currentClip = null;
     this.watcher = null;
@@ -639,8 +304,8 @@ class VideoPage extends Component {
   }
 
   changeVolume(newVolumeValue) {
-    //change youtube and all the audio volume
-    const video = document.getElementById('playerAT');
+    // change youtube and all the audio volume
+    const video = document.getElementById('playerVP');
     video.setVolume(0);
   }
 
@@ -651,7 +316,7 @@ class VideoPage extends Component {
 
   // 1
   render() {
-    // console.log('1 -> render authoring tool')
+    console.log('1 -> render authoring tool')
     return (
       <main id="authoring-tool">
         <div className="w3-row">
@@ -663,7 +328,6 @@ class VideoPage extends Component {
           </div>
         </div>
         <Slider changeVolume={this.changeVolume} />
-        {/*<div>Hello</div>*/}
       </main>
     );
   }
