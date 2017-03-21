d82d792a9bdef6602fcee3d71a5575b60b601371
Rodrigo Leme <lemerodrigo@gmail.com>
2017-03-20 19:09:44 -0700

    Fixed scss importing unexistent file

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,6 @@ import VideoPage from './containers/video-page/VideoPage.jsx';
 import NotFound from './containers/not-found/NotFound.jsx';
 import WishList from './containers/wish-list/WishList.jsx';
 import Disclaimer from './containers/disclaimer/Disclaimer.jsx';
-// import VideoPage from './containers/video-page/VideoPage2.jsx';
 
 ReactDOM.render((
   <Router history={browserHistory}>
