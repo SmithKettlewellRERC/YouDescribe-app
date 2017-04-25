5f5996ab881ffffc8f34e3de6a709cdff2cb15ba
Curt Toppel <ctoppel@aol.com>
2017-04-24 18:16:28 -0700

    verbose commit

---
+++ client/src/components/play-pause-button/PlayPauseButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const PlayPauseButton = (props) => {
+  function playVideo() {
+    props.videoPlayer.playVideo();
+    props.pauseAudioClips();
+  }
+
+  function pauseVideo() {
+    props.videoPlayer.pauseVideo();
+    props.pauseAudioClips();
+  }
+
+  const id = props.videoState === 1 ? 'pause-button' : 'play-button';
+  const onClick = props.videoState === 1 ? pauseVideo : playVideo;
+  const title = props.videoState === 1 ? 'pause button' : 'play button';
+  const action = props.videoState === 1 ? 'pause' : 'play';
+
+
+  return (
+    <div id="play-pause-button">
+      <button autoFocus id={id} onClick={onClick} title={title} accessKey="p">
+        <i className={`fa fa-${action}`} aria-hidden="true" />
+      </button>
+    </div>
+  );
+};
+
+export default PlayPauseButton;
