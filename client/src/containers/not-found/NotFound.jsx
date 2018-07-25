051ecb19000d72e6be3981bdcbb6c1dc5b58b0cc
Rodrigo Leme <lemerodrigo@gmail.com>
2018-07-25 00:03:11 -0300

    Error page

--- client/src/containers/not-found/NotFound.jsx
+++ client/src/containers/not-found/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => (
   <div id="not-found" className="w3-center">
-    <h2>Not Found</h2>
+    <h2>Unfortunately we couldn't find what you are looking for. Try to use the <a href="#search-input">search feature</a> to find some videos of you interest!</h2>
   </div>
 );
 
