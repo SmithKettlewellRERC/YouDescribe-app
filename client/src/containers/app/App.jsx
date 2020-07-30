08ba1ec545bbc11b5845b40769d8207366f53cb7
jcastan6 <josecastanon4@gmail.com>
2020-07-30 06:46:24 -0700

    fixed google analytics

--- client/src/containers/app/App.jsx
+++ client/src/containers/app/App.jsx
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import { browserHistory } from 'react-router';
-import Polyglot from 'node-polyglot';
-import Navbar from '../../components/navbar/Navbar.jsx';
-import Footer from '../../components/footer/Footer.jsx';
-import { ourFetch, getLang } from '../../shared/helperFunctions.js';
-import strings from './../../strings';
-import ReactGA from "react-ga";
+import React, { Component } from "react";
+import { browserHistory } from "react-router";
+import Polyglot from "node-polyglot";
+import Navbar from "../../components/navbar/Navbar.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+import { ourFetch, getLang } from "../../shared/helperFunctions.js";
+import strings from "./../../strings";
 
-const { detect } = require('detect-browser');
-const conf = require('./../../shared/config')();
+const { detect } = require("detect-browser");
+const conf = require("./../../shared/config")();
 const polyglot = new Polyglot({
-    locale: getLang(),
-    phrases: strings[getLang()],
+  locale: getLang(),
+  phrases: strings[getLang()]
 });
 const translate = polyglot.t.bind(polyglot);
-const trackingId = "UA-171142756-3";
-ReactGA.initialize(trackingId);
 
 class App extends Component {
   constructor(props, context) {
@@ -24,16 +21,16 @@ class App extends Component {
 
     this.state = {
       editorTimerValue: 0,
-      searchValue: '',
+      searchValue: "",
 
       // Authentication.
       auth2: null,
       isSignedIn: false,
-      userId: '',
-      userToken: '',
-      userName: '',
-      userPicture: '',
-      userAdmin: 0,
+      userId: "",
+      userToken: "",
+      userName: "",
+      userPicture: "",
+      userAdmin: 0
     };
     this.initGoogleAuth = this.initGoogleAuth.bind(this);
     this.googleSignInSuccess = this.googleSignInSuccess.bind(this);
@@ -55,99 +52,115 @@ class App extends Component {
     const googleUser = this.state.auth2.currentUser.get();
     const googleToken = googleUser.getAuthResponse().id_token;
     ourFetch(`${conf.apiUrl}/auth`, true, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({ googleToken }),
-    })
-    .then((res) => {
-      this.setState({
-        isSignedIn: true,
-        userName: res.result.name,
-        userId: res.result._id,
-        userToken: res.result.token,
-        userPicture: res.result.picture,
-        userAdmin: res.result.admin,
-      }, () => {
-        this.setCookie();
-      });
+      body: JSON.stringify({ googleToken })
+    }).then(res => {
+      this.setState(
+        {
+          isSignedIn: true,
+          userName: res.result.name,
+          userId: res.result._id,
+          userToken: res.result.token,
+          userPicture: res.result.picture,
+          userAdmin: res.result.admin
+        },
+        () => {
+          this.setCookie();
+        }
+      );
     });
   }
 
   // Call back from Google authentication process.
   googleSignInFailure() {
-    console.log('sign in failure');
+    console.log("sign in failure");
   }
 
   initGoogleAuth() {
     const self = this;
-    gapi.load('auth2', function() {
+    gapi.load("auth2", function() {
       const auth2 = gapi.auth2.init({
         client_id: conf.googleClientId,
         fetch_basic_profile: true,
-        scope: 'email profile openid'
+        scope: "email profile openid"
       });
       auth2.isSignedIn.listen(self.signinChanged);
       auth2.then(() => {
-        self.setState({
-          auth2,
-        }, () => {
-          self.state.auth2.attachClickHandler('btn-sign-in', {}, self.googleSignInSuccess, self.googleSignInFailure);
-          self.refreshUserInfo();
-        });
+        self.setState(
+          {
+            auth2
+          },
+          () => {
+            self.state.auth2.attachClickHandler(
+              "btn-sign-in",
+              {},
+              self.googleSignInSuccess,
+              self.googleSignInFailure
+            );
+            self.refreshUserInfo();
+          }
+        );
       });
-    })
+    });
   }
 
   signinChanged(val) {
-      // console.log('Signin state changed to ', val);
+    // console.log('Signin state changed to ', val);
   }
 
   refreshUserInfo() {
     const self = this;
     this.state.auth2.then(() => {
       if (this.state.auth2.isSignedIn.get()) {
-        const userId = this.getCookie('userId');
-        const userToken = this.getCookie('userToken');
-        const userName = this.getCookie('userName');
-        const userPicture = this.getCookie('userPicture');
+        const userId = this.getCookie("userId");
+        const userToken = this.getCookie("userToken");
+        const userName = this.getCookie("userName");
+        const userPicture = this.getCookie("userPicture");
         if (userId && userToken && userName && userPicture) {
-          self.setState({
-            isSignedIn: true,
-            userName,
-            userId,
-            userToken,
-            userPicture,
-          }, () => {
-            self.setCookie();
-          });
+          self.setState(
+            {
+              isSignedIn: true,
+              userName,
+              userId,
+              userToken,
+              userPicture
+            },
+            () => {
+              self.setCookie();
+            }
+          );
         } else {
-          self.setState({
-            isSignedIn: false,
-            userName: '',
-            userId: '',
-            userToken: '',
-            userPicture: '',
-          }, () => {
-            self.resetCookie();
-          });
+          self.setState(
+            {
+              isSignedIn: false,
+              userName: "",
+              userId: "",
+              userToken: "",
+              userPicture: ""
+            },
+            () => {
+              self.resetCookie();
+            }
+          );
         }
       }
     });
   }
 
   getUserInfo() {
-    const userId = this.getCookie('userId');
-    const userToken = this.getCookie('userToken');
-    const userName = this.getCookie('userName');
-    const userPicture = this.getCookie('userPicture');
+    const userId = this.getCookie("userId");
+    const userToken = this.getCookie("userToken");
+    const userName = this.getCookie("userName");
+    const userPicture = this.getCookie("userPicture");
     if (userId && userToken && userName && userPicture) {
       return {
         userId,
         userName,
         userToken,
-        userPicture,
+        userPicture
       };
     }
     return null;
@@ -155,16 +168,19 @@ class App extends Component {
 
   signOut() {
     this.state.auth2.signOut().then(() => {
-      this.setState({
-        isSignedIn: false,
-        userName: '',
-        userId: '',
-        userToken: '',
-      }, () => {
-        this.resetCookie();
-        this.initGoogleAuth();
-        location.href = '/';
-      });
+      this.setState(
+        {
+          isSignedIn: false,
+          userName: "",
+          userId: "",
+          userToken: ""
+        },
+        () => {
+          this.resetCookie();
+          this.initGoogleAuth();
+          location.href = "/";
+        }
+      );
     });
   }
 
@@ -194,35 +210,38 @@ class App extends Component {
   }
 
   getCookie(cname) {
-    const name = cname + '=';
+    const name = cname + "=";
     const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+    const ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    return '';
+    return "";
   }
 
   componentDidMount() {
     const browser = detect();
-    const unsupportedOs = ['ios', 'Android OS'];
-    const supportedBrowsers = ['chrome', 'firefox'];
-    if (unsupportedOs.indexOf(browser.os) !== -1 || supportedBrowsers.indexOf(browser.name) === -1) {
+    const unsupportedOs = ["ios", "Android OS"];
+    const supportedBrowsers = ["chrome", "firefox"];
+    if (
+      unsupportedOs.indexOf(browser.os) !== -1 ||
+      supportedBrowsers.indexOf(browser.name) === -1
+    ) {
       browserHistory.push(`/unsupported-browser`);
     } else {
-      window.addEventListener('google-auth-lib-loaded', this.initGoogleAuth);
+      window.addEventListener("google-auth-lib-loaded", this.initGoogleAuth);
     }
   }
 
   clickHandler(searchValue) {
     this.setState({
-      searchValue,
+      searchValue
     });
     const q = encodeURIComponent(searchValue);
     browserHistory.push(`/search?q=${q}`);
@@ -241,7 +260,7 @@ class App extends Component {
           getAppState: this.getAppState,
           getVideoProgress: this.getVideoProgress,
           translate: translate,
-          getUserInfo: this.getUserInfo,
+          getUserInfo: this.getUserInfo
         })}
         <Footer translate={translate} />
       </div>
