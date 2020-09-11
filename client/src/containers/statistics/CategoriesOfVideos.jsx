b3ebb7c31646e2917bee0bd021af28b8018bcf32
jcastan6 <josecastanon4@gmail.com>
2020-09-11 08:16:49 -0700

    changed api key to receive from backend

--- client/src/containers/statistics/CategoriesOfVideos.jsx
+++ client/src/containers/statistics/CategoriesOfVideos.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import ReactEcharts from "echarts-for-react";
 import Echarts from "echarts";
-import { ButtonToolbar, Button, FormControl, Form, Table } from "react-bootstrap";
+import ToolkitProvider from "react-bootstrap-table2-toolkit";
+
+import {
+  ButtonToolbar,
+  Button,
+  FormControl,
+  Form,
+  Table
+} from "react-bootstrap";
 import AdminNav from "../../components/admin-nav/AdminNav.jsx";
-import { ourFetchWithToken, convertTimeToCardFormat, convertViewsToCardFormat } from "../../shared/helperFunctions";
+import {
+  ourFetchWithToken,
+  convertTimeToCardFormat,
+  convertViewsToCardFormat
+} from "../../shared/helperFunctions";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import BootstrapTable from "react-bootstrap-table-next";
 const conf = require("../../shared/config")();
 
 // http://www.echartsjs.com/examples/en/editor.html?c=dataset-simple1
@@ -15,17 +27,138 @@ class CategoriesOfVideos extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: (parseInt(props.location.query.startdate) || (new Date(conf.startDateTimeStamp)).getTime()),
-      endDate: (parseInt(props.location.query.enddate) || (new Date()).getTime()), 
+      startDate:
+        parseInt(props.location.query.startdate) ||
+        new Date(conf.startDateTimeStamp).getTime(),
+      endDate: parseInt(props.location.query.enddate) || new Date().getTime(),
       categories: [],
       source: [],
-      showLoading: true,
+      showLoading: true
     };
 
     this.handleStartDateChange = this.handleStartDateChange.bind(this);
     this.handleEndDateChange = this.handleEndDateChange.bind(this);
     this.handleReload = this.handleReload.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.createTable = this.createTable.bind(this);
+  }
+
+  createTable() {
+    const columns = [
+      {
+        dataField: "category",
+        text: "Category",
+        headerStyle: () => {
+          return { width: "40%" };
+        }
+      },
+      {
+        dataField: "described",
+        text: "Described",
+        headerStyle: () => {
+          return { width: "30%" };
+        },
+        sort: true
+      },
+      {
+        dataField: "wishlist-described",
+        text: "Described from wishlist",
+        searchable: false,
+        headerStyle: () => {
+          return { width: "10%" };
+        },
+        sort: true
+      },
+      {
+        dataField: "wishlist-not-described",
+        text: "In Wishlist (not yet described)",
+        searchable: false,
+        headerStyle: () => {
+          return { width: "10%" };
+        },
+        sort: true
+      }
+    ];
+
+    const sizePerPageRenderer = ({
+      options,
+      currSizePerPage,
+      onSizePerPageChange
+    }) => (
+      <ButtonToolbar>
+        <DropdownButton
+          drop="down"
+          title={currSizePerPage}
+          variant="secondary"
+          className="pagination-style"
+        >
+          {options.map(option => {
+            const isSelect = currSizePerPage === `${option.page}`;
+            return (
+              <Dropdown.Item
+                key={option.text}
+                type="button"
+                onClick={() => onSizePerPageChange(option.page)}
+                className={`btn ${
+                  isSelect ? "btn-secondary" : "btn-secondary"
+                }`}
+              >
+                {option.text}
+              </Dropdown.Item>
+            );
+          })}
+        </DropdownButton>
+      </ButtonToolbar>
+    );
+
+    const pageButtonRenderer = ({ page, onPageChange }) => {
+      const handleClick = e => {
+        e.preventDefault();
+        onPageChange(page);
+      };
+      return (
+        <div className="div-pagination">
+          <Button className="pagination-button">
+            <a href="#" onClick={handleClick} className="pagination-href">
+              {page}
+            </a>
+          </Button>
+        </div>
+      );
+    };
+
+    // object that wraps styling of the given pagination components,
+    // which includes the sizePerPageRenderer (left side) and
+    // pageButtonRenderer (right side)
+    const options = {
+      sizePerPageRenderer,
+      pageButtonRenderer
+    };
+
+    return (
+      <div className="wrapper_room">
+        <Table scrollY maxHeight="70vh" striped boredered centered small>
+          <ToolkitProvider
+            keyField="roomName"
+            data={this.state.source}
+            columns={columns}
+            search
+          >
+            {props => (
+              <div>
+                <center>
+                  <h3>videos by category (click row to sort)</h3>
+                </center>
+
+                <hr />
+
+                <BootstrapTable {...props.baseProps} />
+              </div>
+            )}
+          </ToolkitProvider>
+        </Table>
+      </div>
+    );
   }
 
   componentWillMount() {
@@ -35,19 +168,19 @@ class CategoriesOfVideos extends Component {
 
   loadCategories() {
     const url = `${conf.apiUrl}/statistics/getcategories`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       const items = response.result;
       var categories = [];
       items.forEach(item => {
         categories[item.title] = {
-          "category": item.title,
-          "described": 0,
+          category: item.title,
+          described: 0,
           "wishlist-described": 0,
-          "wishlist-not-described": 0,
+          "wishlist-not-described": 0
         };
       });
       this.setState({
-        categories: categories,
+        categories: categories
       });
       this.loadStatistics(categories);
     });
@@ -56,9 +189,10 @@ class CategoriesOfVideos extends Component {
   loadStatistics(categories) {
     const url = `${conf.apiUrl}/statistics/getcategoriesofvideos?startdate=${this.state.startDate}&enddate=${this.state.endDate}`;
     var source = [];
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       const wishListDescribedVideos = response.result.wishListDescribedVideos;
-      const wishListNotDescribedVideos = response.result.wishListNotDescribedVideos;
+      const wishListNotDescribedVideos =
+        response.result.wishListNotDescribedVideos;
       const describedVideos = response.result.describedVideos;
       for (var i = 0; i < describedVideos.length; ++i) {
         var key = describedVideos[i].category;
@@ -73,13 +207,17 @@ class CategoriesOfVideos extends Component {
         categories[key]["wishlist-not-described"]++;
       }
       for (key in categories) {
-        if (categories[key]["described"] || categories[key]["wishlist-described"] || categories[key]["wishlist-not-described"]) {
+        if (
+          categories[key]["described"] ||
+          categories[key]["wishlist-described"] ||
+          categories[key]["wishlist-not-described"]
+        ) {
           source.push(categories[key]);
         }
       }
       this.setState({
         source: source,
-        showLoading: false,
+        showLoading: false
       });
     });
   }
@@ -91,50 +229,55 @@ class CategoriesOfVideos extends Component {
       },
       grid: {
         left: "12%",
-        right: "3%",
+        right: "3%"
       },
       tooltip: {},
       legend: {},
       dataset: {
-        dimensions: ["category", "described", "wishlist-described", "wishlist-not-described"],
-        source: this.state.source,
+        dimensions: [
+          "category",
+          "described",
+          "wishlist-described",
+          "wishlist-not-described"
+        ],
+        source: this.state.source
       },
       xAxis: {},
       yAxis: {
-        type: "category",
+        type: "category"
       },
-      series: [
-        {type: 'bar'},
-        {type: 'bar'},
-        {type: 'bar'},
-      ],
+      series: [{ type: "bar" }, { type: "bar" }, { type: "bar" }]
     };
   }
 
   getOnEvents() {
     return {
-      click: this.handleClick,
-    }
+      click: this.handleClick
+    };
   }
 
   handleClick(params) {
     window.open(
-      `/statistics/tagsofvideos?category=${encodeURIComponent(params.name)}&group=${params.seriesName}&startdate=${this.state.startDate}&enddate=${this.state.endDate}`,
-      "_blank",
+      `/statistics/tagsofvideos?category=${encodeURIComponent(
+        params.name
+      )}&group=${params.seriesName}&startdate=${this.state.startDate}&enddate=${
+        this.state.endDate
+      }`,
+      "_blank"
     );
   }
 
   handleStartDateChange(startDate) {
     this.setState({
-      startDate: startDate.getTime(),
+      startDate: startDate.getTime()
     });
-  };
+  }
 
   handleEndDateChange(endDate) {
     this.setState({
-      endDate: endDate.getTime(),
+      endDate: endDate.getTime()
     });
-  };
+  }
 
   handleReload() {
     window.location.href = `/statistics/categoriesofvideos?startdate=${this.state.startDate}&enddate=${this.state.endDate}`;
@@ -143,40 +286,50 @@ class CategoriesOfVideos extends Component {
   render() {
     return (
       <div className="admin-responsive">
-        <AdminNav/>
+        <AdminNav />
         <main className="w3-row">
-          <div style={{textAlign: "center", fontSize: 20}}><b>Number Of Videos Per Category</b></div>
-          <div style={{textAlign: "center"}}>
-            <div style={{display: "inline-block"}}>
+          <div style={{ textAlign: "center", fontSize: 20 }}>
+            <b>Number Of Videos Per Category</b>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <div style={{ display: "inline-block" }}>
               Start Date:&nbsp;
               <DatePicker
                 placeholderText="Start Date"
                 selected={this.state.startDate}
-                onChange={(value) => this.handleStartDateChange(value)}
+                onChange={value => this.handleStartDateChange(value)}
               />
             </div>
-            <span style={{marginLeft: 10, marginRight: 10}}></span>
-            <div style={{display: "inline-block"}}>
+            <span style={{ marginLeft: 10, marginRight: 10 }}></span>
+            <div style={{ display: "inline-block" }}>
               End Date:&nbsp;
               <DatePicker
                 placeholderText="End Date"
                 selected={this.state.endDate}
-                onChange={(value) => this.handleEndDateChange(value)}
+                onChange={value => this.handleEndDateChange(value)}
               />
             </div>
-            <span style={{marginLeft: 10, marginRight: 10}}></span>
-            <Button type="submit" style={{marginTop: 20, marginBottom: 20}} variant="outline-info" onClick={this.handleReload}>Reload</Button>
+            <span style={{ marginLeft: 10, marginRight: 10 }}></span>
+            <Button
+              type="submit"
+              style={{ marginTop: 20, marginBottom: 20 }}
+              variant="outline-info"
+              onClick={this.handleReload}
+            >
+              Reload
+            </Button>
           </div>
-          <ReactEcharts 
+          <ReactEcharts
             option={this.getOption()}
             onEvents={this.getOnEvents()}
-            style={{height: "750px", width: "100%"}}
+            style={{ height: "750px", width: "100%" }}
             className="react_for_echarts"
             showLoading={this.state.showLoading}
           />
+          {this.createTable()}
         </main>
       </div>
     );
-  };
+  }
 }
 export default CategoriesOfVideos;
--- client/src/containers/statistics/SummaryOfTimeRange.jsx
+++ client/src/containers/statistics/SummaryOfTimeRange.jsx
@@ -2,9 +2,19 @@ import React, { Component } from "react";
 import ReactEcharts from "echarts-for-react";
 import Echarts from "echarts";
 import { Link } from "react-router";
-import { ButtonToolbar, Button, FormControl, Form, Table } from "react-bootstrap";
+import {
+  ButtonToolbar,
+  Button,
+  FormControl,
+  Form,
+  Table
+} from "react-bootstrap";
 import AdminNav from "../../components/admin-nav/AdminNav.jsx";
-import { ourFetchWithToken, convertTimeToCardFormat, convertViewsToCardFormat } from "../../shared/helperFunctions";
+import {
+  ourFetchWithToken,
+  convertTimeToCardFormat,
+  convertViewsToCardFormat
+} from "../../shared/helperFunctions";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -15,25 +25,27 @@ class SummaryOfTimeRange extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: (parseInt(props.location.query.startdate) || (new Date(conf.startDateTimeStamp)).getTime()),
-      endDate: (parseInt(props.location.query.enddate) || (new Date()).getTime()), 
+      startDate:
+        parseInt(props.location.query.startdate) ||
+        new Date(conf.startDateTimeStamp).getTime(),
+      endDate: parseInt(props.location.query.enddate) || new Date().getTime(),
       videoVisits: "loading...",
       webVisits: "loading...",
       users: "loading...",
       audioClips: {
         count: "loading...",
-        duration: "loading...",
+        duration: "loading..."
       },
       audioDescriotions: "loading...",
       videos: {
         count: "loading...",
-        duration: "loading...",
+        duration: "loading..."
       },
       words: "loading...",
       topVideos: ["loading..."],
-      feedbacks: ["loading..."],
+      feedbacks: ["loading..."]
     };
-    
+
     this.handleStartDateChange = this.handleStartDateChange.bind(this);
     this.handleEndDateChange = this.handleEndDateChange.bind(this);
     this.handleReload = this.handleReload.bind(this);
@@ -57,14 +69,14 @@ class SummaryOfTimeRange extends Component {
 
   loadCountOfVisits(startDate, endDate) {
     const url = `${conf.apiUrl}/statistics/getcountofvisits?startdate=${startDate}&enddate=${endDate}`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       const items = response.result;
       const topVideos = [];
       items.topVideos.forEach(item => {
         topVideos.push(
           <div key={item._id}>
-            <Link to={"/video/" + item._id}
-              target={item._id}>{item.video.title}
+            <Link to={"/video/" + item._id} target={item._id}>
+              {item.video.title}
             </Link>
           </div>
         );
@@ -72,197 +84,224 @@ class SummaryOfTimeRange extends Component {
       this.setState({
         videoVisits: items.countOfVideoVisits,
         webVisits: items.countOfWebVisits,
-        topVideos: topVideos,
+        topVideos: topVideos
       });
     });
   }
 
   loadCountOfFeedbacks(startDate, endDate) {
     const url = `${conf.apiUrl}/statistics/getcountoffeedbacks?startdate=${startDate}&enddate=${endDate}`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       const items = response.result;
       const feedbacks = [];
       items.forEach(feedback => {
         feedbacks.push(
-          <div key={feedback._id}>{conf.audioDescriptionFeedbacks[feedback._id]}</div>
+          <div key={feedback._id}>
+            {conf.audioDescriptionFeedbacks[feedback._id]}
+          </div>
         );
       });
       this.setState({
-        feedbacks: feedbacks,
+        feedbacks: feedbacks
       });
     });
   }
 
   loadCountOfUsers(startDate, endDate) {
     const url = `${conf.apiUrl}/statistics/getcountofusers?startdate=${startDate}&enddate=${endDate}`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       this.setState({
-        users: response.result,
+        users: response.result
       });
     });
   }
 
   loadCountOfAudioClips(startDate, endDate) {
     const url = `${conf.apiUrl}/statistics/getcountofaudioclips?startdate=${startDate}&enddate=${endDate}`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       this.setState({
-        audioClips: response.result,
+        audioClips: response.result
       });
     });
   }
 
   loadCountOfAudioDescriptions(startDate, endDate) {
     const url = `${conf.apiUrl}/statistics/getcountofaudiodescriptions?startdate=${startDate}&enddate=${endDate}`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       this.setState({
-        audioDescriotions: response.result.count,
+        audioDescriotions: response.result.count
       });
     });
   }
 
   loadCountOfVideos(startDate, endDate) {
     const url = `${conf.apiUrl}/statistics/getcountofvideos?startdate=${startDate}&enddate=${endDate}`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       this.setState({
-        videos: response.result,
+        videos: response.result
       });
     });
   }
 
   loadCountOfWords(startDate, endDate) {
     const url = `${conf.apiUrl}/statistics/getcountofwords?startdate=${startDate}&enddate=${endDate}`;
-    ourFetchWithToken(url).then((response) => {
+    ourFetchWithToken(url).then(response => {
       this.setState({
-        words: response.result.count,
+        words: response.result.count
       });
     });
   }
 
   handleStartDateChange(startDate) {
     this.setState({
-      startDate: startDate.getTime(),
+      startDate: startDate.getTime()
     });
-  };
+  }
 
   handleEndDateChange(endDate) {
     this.setState({
-      endDate: endDate.getTime(),
+      endDate: endDate.getTime()
     });
-  };
+  }
 
   handleReload() {
     window.location.href = `/statistics/summaryoftimerange?startdate=${this.state.startDate}&enddate=${this.state.endDate}`;
   }
 
   getRoute(type) {
-    return `/statistics/dailycountofdatarecords?type=${encodeURIComponent(type)}&startdate=${this.state.startDate}&enddate=${this.state.endDate}`;
+    return `/statistics/dailycountofdatarecords?type=${encodeURIComponent(
+      type
+    )}&startdate=${this.state.startDate}&enddate=${this.state.endDate}`;
   }
 
   render() {
     return (
       <div className="admin-responsive">
-        <AdminNav/>
+        <AdminNav />
         <main className="w3-row">
-          <div style={{textAlign: "center", fontSize: 20}}><b>Summary Of Time Range</b></div>
-          <div style={{textAlign: "center"}}>
-            <div style={{display: "inline-block"}}>
+          <div style={{ textAlign: "center", fontSize: 20 }}>
+            <b>Summary Of Time Range</b>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <div style={{ display: "inline-block" }}>
               Start Date:&nbsp;
               <DatePicker
                 placeholderText="Start Date"
                 selected={this.state.startDate}
-                onChange={(value) => this.handleStartDateChange(value)}
+                onChange={value => this.handleStartDateChange(value)}
               />
             </div>
-            <span style={{marginLeft: 10, marginRight: 10}}></span>
-            <div style={{display: "inline-block"}}>
+            <span style={{ marginLeft: 10, marginRight: 10 }}></span>
+            <div style={{ display: "inline-block" }}>
               End Start:&nbsp;
               <DatePicker
                 placeholderText="End Date"
                 selected={this.state.endDate}
-                onChange={(value) => this.handleEndDateChange(value)}
+                onChange={value => this.handleEndDateChange(value)}
               />
             </div>
-            <span style={{marginLeft: 10, marginRight: 10}}></span>
-            <Button type="submit" style={{marginTop: 20, marginBottom: 20}} variant="outline-info" onClick={this.handleReload}>Reload</Button>
+            <span style={{ marginLeft: 10, marginRight: 10 }}></span>
+            <Button
+              type="submit"
+              style={{ marginTop: 20, marginBottom: 20 }}
+              variant="outline-info"
+              onClick={this.handleReload}
+            >
+              Reload
+            </Button>
           </div>
-          <Table striped bordered hover style={{textAlign: "center"}}>
-          <thead>
-            <tr>
-              <th style={{width: "50%"}}>Item</th>
-              <th>Summary</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>Count Of Video Visits</th>
-              <th>
-                {this.state.videoVisits}&nbsp;
-                <Link to={this.getRoute("Video Visits")} target="daily">View Daily Count</Link>
-              </th>
-            </tr>
-            <tr>
-              <th>Count Of Web Visits</th>
-              <th>
-                {this.state.webVisits}&nbsp;
-                <Link to={this.getRoute("Web Visits")} target="daily">View Daily Count</Link>
-              </th>
-            </tr>
-            <tr>
-              <th>Count Of Users</th>
-              <th>
-                {this.state.users}&nbsp;
-                <Link to={this.getRoute("Users")} target="daily">View Daily Count</Link>
-              </th>
-            </tr>
-            <tr>
-              <th>Count Of Audio Clips</th>
-              <th>
-                {this.state.audioClips.count}&nbsp;
-                <Link to={this.getRoute("Audio Clips")} target="daily">View Daily Count</Link>
-              </th>
-            </tr>
-            <tr>
-              <th>Count Of Audio Descriptions</th>
-              <th>
-                {this.state.audioDescriotions}&nbsp;
-                <Link to={this.getRoute("Audio Descriptions")} target="daily">View Daily Count</Link>
-              </th>
-            </tr>
-            <tr>
-              <th>Count Of Videos</th>
-              <th>
-                {this.state.videos.count}&nbsp;
-                <Link to={this.getRoute("Videos")} target="daily">View Daily Count</Link>
-              </th>
-            </tr>
-            <tr>
-              <th>Count Of Words</th>
-              <th>
-                {this.state.words}&nbsp;
-                <Link to={this.getRoute("Words")} target="daily">View Daily Count</Link>
-              </th>
-            </tr>
-            <tr>
-              <th>Duration Of Audio Clips (s)</th>
-              <th>{this.state.audioClips.duration}</th>
-            </tr>
-            <tr>
-              <th>Duration Of Videos (s)</th>
-              <th>{this.state.videos.duration}</th>
-            </tr>
-            <tr>
-              <th>Most Viewed Videos</th>
-              <th>{this.state.topVideos}</th>
-            </tr>
-            <tr>
-              <th>Most Frequent Feedbacks</th>
-              <th>{this.state.feedbacks}</th>
-            </tr>
-          </tbody>
+          <Table striped bordered hover style={{ textAlign: "center" }}>
+            <thead>
+              <tr>
+                <th style={{ width: "50%" }}>Item</th>
+                <th>Summary</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Count Of Video Visits</th>
+                <th>
+                  {this.state.videoVisits}&nbsp;
+                  <Link to={this.getRoute("Video Visits")} target="daily">
+                    View Daily Count
+                  </Link>
+                </th>
+              </tr>
+              <tr>
+                <th>Count Of Home Page Visits</th>
+                <th>
+                  {this.state.webVisits}&nbsp;
+                  <Link to={this.getRoute("Web Visits")} target="daily">
+                    View Daily Count
+                  </Link>
+                </th>
+              </tr>
+              <tr>
+                <th>Count of User Logins</th>
+                <th>
+                  {this.state.users}&nbsp;
+                  <Link to={this.getRoute("Users")} target="daily">
+                    View Daily Count
+                  </Link>
+                </th>
+              </tr>
+              <tr>
+                <th>Count Of Audio Clips</th>
+                <th>
+                  {this.state.audioClips.count}&nbsp;
+                  <Link to={this.getRoute("Audio Clips")} target="daily">
+                    View Daily Count
+                  </Link>
+                </th>
+              </tr>
+              <tr>
+                <th>Count Of Audio Descriptions</th>
+                <th>
+                  {this.state.audioDescriotions}&nbsp;
+                  <Link to={this.getRoute("Audio Descriptions")} target="daily">
+                    View Daily Count
+                  </Link>
+                </th>
+              </tr>
+              <tr>
+                <th>Count Of Videos</th>
+                <th>
+                  {this.state.videos.count}&nbsp;
+                  <Link to={this.getRoute("Videos")} target="daily">
+                    View Daily Count
+                  </Link>
+                </th>
+              </tr>
+              <tr>
+                <th>Count Of Words</th>
+                <th>
+                  {this.state.words}&nbsp;
+                  <Link to={this.getRoute("Words")} target="daily">
+                    View Daily Count
+                  </Link>
+                </th>
+              </tr>
+              <tr>
+                <th>Duration Of Audio Clips (s)</th>
+                <th>{this.state.audioClips.duration}</th>
+              </tr>
+              <tr>
+                <th>Duration Of Videos (s)</th>
+                <th>{this.state.videos.duration}</th>
+              </tr>
+              <tr>
+                <th>Most Viewed Videos</th>
+                <th>{this.state.topVideos}</th>
+              </tr>
+              <tr>
+                <th>Most Frequent Feedbacks</th>
+                <th>{this.state.feedbacks}</th>
+              </tr>
+            </tbody>
           </Table>
         </main>
       </div>
     );
-  };
+  }
 }
 export default SummaryOfTimeRange;
