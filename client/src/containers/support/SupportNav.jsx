a7db1c53b65f5b4cc2ae73c107caf90866ae74bf
jcastan6 <josecastanon4@gmail.com>
2022-01-30 23:16:19 -0800

    fixed file name issue

--- client/src/containers/support/SupportNav.jsx
+++ client/src/containers/support/SupportNav.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from "react";
+import { Link } from "react-router";
 
 const SupportNav = (props) => (
-    <ul>
-    <li><Link to="/support/about">{props.translate('General information about YouDescribe')}</Link></li>
-    <li><Link to="/support/viewers">{props.translate('FAQ pages for viewers')}</Link></li>
-    <li><Link to="/support/describers">{props.translate('FAQ for describers')}</Link></li>
-    <li><Link to="/support/tutorial">{props.translate('A step-by-step audio description tutorial with a trouble shooting section')}</Link></li>
-    <li><Link to="/support/privacy">Privacy Policy</Link></li>
-    </ul>
+  <ul>
+    <li>
+      <Link to="/support/about">
+        {props.translate("General information about YouDescribe")}
+      </Link>
+    </li>
+    <li>
+      <Link to="/support/viewers">
+        {props.translate("FAQ pages for viewers")}
+      </Link>
+    </li>
+    <li>
+      <Link to="/support/describers">
+        {props.translate("FAQ for describers")}
+      </Link>
+    </li>
+    <li>
+      <Link to="/support/tutorial">
+        {props.translate(
+          "A step-by-step audio description tutorial with a trouble shooting section"
+        )}
+      </Link>
+    </li>
+    <li>
+      <Link to="/support/privacy">Privacy Policy</Link>
+    </li>
+  </ul>
 );
-
 export default SupportNav;
